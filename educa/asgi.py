22e2174e7ba6121c20a0bb59aba5104afc28f6d0
leroyg <leroy.gardner@gmail.com>
2021-08-21 14:42:32 -0500

    fixed naming

--- educa/asgi.py
+++ educa/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for onBoardingPlatform project.
+ASGI config for educa project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onBoardingPlatform.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
 
 application = get_asgi_application()
